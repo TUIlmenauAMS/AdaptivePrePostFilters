e42e533ada0946a0c14f5411032aee538084ad56
gdtschuller <gerald.schuller@tu-ilmenau.de>
2023-05-30 16:32:03 +0200

    Update inputFile.py

--- inputFile.py
+++ inputFile.py
@@ -7,6 +7,6 @@ def fileDialog():
     file_path = filedialog.askopenfilename()
     string='.wav'
     if file_path.find(string, len(file_path)-5, len(file_path)) == -1:
-        print "Not a .wav file, Openning Default file"
+        print("Not a .wav file, Openning Default file")
         file_path='sc03_16m.wav'
     return file_path
